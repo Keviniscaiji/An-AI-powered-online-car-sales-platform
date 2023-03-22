0f2a1886504fe91fcf5d6cec352772c8c39c0dd9
KaiwenGong <1724916415@qq.com>
2023-03-22 20:25:05 +0800

    making the login page

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
-# mail = Mail()
+mail = Mail()
 # moment = Moment()
 db = SQLAlchemy()
 
--- run.py
+++ run.py
@@ -1,10 +1,21 @@
+from flask import request
+
 from app import create_app, db
 from flask_migrate import Migrate
+from flask_babel import Babel, gettext as _
 """
 Entrance of this web project
 """
 
 app = create_app('development')
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['zh', 'en'])
+
 
 # -------- Remote Server Deployment Configuration -------- #
 HOST = '127.0.0.1'
