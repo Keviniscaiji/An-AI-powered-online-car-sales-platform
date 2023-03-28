355c8ddbc27154ed695b794b9a42074ed3e64354
KaiwenGong <1724916415@qq.com>
2023-03-28 15:52:51 +0800

    completing the blank template

--- app/chat/views.py
+++ app/chat/views.py
@@ -2,6 +2,8 @@ from flask import render_template, session, redirect, url_for, request
 from . import chat
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
 # chatbot = ChatBot("My ChatterBot")
 # trainer = ChatterBotCorpusTrainer(chatbot)
 # trainer.train("chatterbot.corpus.english")
@@ -11,9 +13,15 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 #     user_text = request.args.get("msg")
 #     return str(chatbot.get_response(user_text))
 
+@chat.route("/get")
+def get_bot_response():
+    return str("Nice question")
+
+
 @chat.route('/admin_chat')
 def admin_chat():
     return render_template('chat/admin_chat.html')
+
 
 @chat.route('/customer_chat')
 def customer_chat():
