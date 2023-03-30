aa06252af6db5e5ad8186981104e04bbebabbfca
KaiwenGong <1724916415@qq.com>
2023-03-30 09:42:53 +0800

    completing the blank template

--- app/autoChat/views.py
+++ app/autoChat/views.py
@@ -1,10 +1,12 @@
-from flask import render_template, session, redirect, url_for, request
+from flask import render_template, session, redirect, url_for, request, jsonify
 from . import autoChat
 from nltk.chat.util import Chat, reflections
 
-@autoChat.route('/autoChat/index', methods=['POST','GET'])
+
+@autoChat.route('/autoChat/index', methods=['POST', 'GET'])
 def index():
     return render_template('autoChat/index.html')
+
 
 # 加载聊天数据
 
@@ -19,19 +21,17 @@ pairs = [
 # 创建 Chat 对象
 chatbot = Chat(pairs, reflections)
 
+
 # 定义聊天接口
-@autoChat.route('/chat', methods=['POST','GET'])
+@autoChat.route('/chat', methods=['POST', 'GET'])
 def chat():
     # 获取用户输入的文本
-    user_text = request.form['text']
-
+    user_text = request.form['message']
     # 使用 Chat 对象处理用户输入，返回聊天结果
-    response_text = chatbot.respond(user_text)
-
-    # 返回回复
-    return user_text
-
-    # 使用聊天数据处理用户输入，返回
+    response_text = str(chatbot.respond(user_text))
+    # return the reply
+    # return "response_text"
+    return jsonify({'text': "?"})
 
 #
 #
